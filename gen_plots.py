3790efa107ce28daa72dad7debdff0c5de0d3cbd
danst <dan.steinmaurer@gmail.com>
2025-05-16 11:38:46 +0200

    fix interpolation rate

--- gen_plots.py
+++ gen_plots.py
@@ -4,13 +4,13 @@ import TP2.poly as chp
 import numpy as np
 import scipy.special as sci_sp
 
-def generate_plot(f, c_poly, N, fname, start=0, end=1, cheberchev=True):
+def generate_plot(f, c_poly, N, fname, ylabel="", start=-1, end=1, cheberchev=True):
     #calculate reference data
     X = np.linspace(start, end, N)
     Yint = f(X)
 
     # for cheberchev nodes
-    X_c = np.array([np.pi/N * j for j in range(N)])
+    X_c = (start+end)/2 + (end -start)/2*np.cos(np.array([np.pi/N * j for j in range(N+1)]))
     Y_c  = f(X_c)
 
     # generate lagrange polynomial
@@ -26,10 +26,12 @@ def generate_plot(f, c_poly, N, fname, start=0, end=1, cheberchev=True):
     Ylap2 = F2(Xc)
     Y_cheb = c_poly(Xc)
 
-    fig, (ax1, ax2) = plt.subplots(2,1)
+    fig, (ax1, ax2) = plt.subplots(2,1, figsize=(10, 6))
 
-    ax1.set_ylabel("$\sin(\pi/2 \cdot x)$")
-    ax1.scatter(X, Yint)
+    ax1.set_ylabel(ylabel)
+    ax1.scatter(X, Yint, label="Lineare Punkte")
+    ax1.scatter(X_c, Y_c, label="Cheberchev Knoten")
+    ax1.legend()
     ax1.plot(Xc, Ylap, label="Lagrange Interpolation")
     ax1.plot(Xc, Ylap2, label="Lagrange Interpolation | Cheberchev Knoten")
     if cheberchev:
@@ -43,6 +45,7 @@ def generate_plot(f, c_poly, N, fname, start=0, end=1, cheberchev=True):
         ax2.plot(Xc, (Yref - Y_cheb)/Yref, label="Cheberchev Polynome")
 
     ax1.legend()
+    print(fname)
     plt.savefig(fname)
 
 
@@ -50,18 +53,22 @@ generate_plot(
     lambda x: np.sin(np.pi/2 *x),
     chp.sinpih,
     13,
-    "plots/sinpi.pdf"
+    "plots/sinpi.pdf",
+    r"$sin(\pi/2 \cdot x )$"
 )
 generate_plot(
     np.arctan,
     chp.arctan,
     15,
     "plots/arctan.pdf",
-    #    cheberchev=False
+    r"$arctan(x)$"
 )
 generate_plot(
     lambda x: sci_sp.gamma(x+1),
     chp.gammap1,
     15,
-    "plots/gamma.pdf"
+    "plots/gamma.pdf",
+    r"$\Gamma(x+1) = x!$",
+    start=0,
+    end=1
 )
